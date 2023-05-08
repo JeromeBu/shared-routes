d972854923ba51342d8e116e59a578eb39b053f3
Jérôme Burkard <burkardjerome@gmail.com>
2023-05-08 10:23:14 +0200

    updates after ownership transfer from joseph, now using ts-ci

---
+++ test/defineRoutes.test.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { describe, it, expect } from "vitest";
+import { defineRoutes, defineRoute, listRoutes } from "../src/core";
+
+describe("Shared routes definitions", () => {
+  describe("defineRoutes", () => {
+    it("does not allow 2 routes with same method and url", () => {
+      const createMySharedRoutes = () =>
+        defineRoutes({
+          addBook: defineRoute({
+            method: "post",
+            url: "/books",
+            bodySchema: z.object({ title: z.string() }),
+          }),
+          getAllBooks: defineRoute({
+            method: "post",
+            url: "/books",
+            responseBodySchema: z.array(z.object({ id: z.string(), name: z.string() })),
+          }),
+        });
+
+      expect(createMySharedRoutes).toThrowError(
+        new Error(
+          "You cannot have several routes with same http method and url, got: POST /books twice (at least)",
+        ),
+      );
+    });
+
+    it("create routes with the expected types and shows list of routes", () => {
+      const routes = defineRoutes({
+        addBook: defineRoute({
+          method: "post",
+          url: "/books",
+          bodySchema: z.object({ title: z.string() }),
+        }),
+        getAllBooks: defineRoute({
+          method: "get",
+          url: "/books",
+          queryParamsSchema: z.object({ lala: z.string() }),
+          responseBodySchema: z.array(z.object({ id: z.string(), name: z.string() })),
+        }),
+      });
+
+      expect(() => routes.getAllBooks.bodySchema.parse({ yo: "lala" })).toThrow();
+      expect(listRoutes(routes)).toEqual(["POST /books", "GET /books"]);
+    });
+  });
+});
