7a6b6bd645f997c9f726c1661832648ea45ddecc
actions <actions@github.com>
2023-07-14 11:25:03 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ esm/fetch/queryParamsToString.mjs
@@ -0,0 +1,33 @@
+var __read = (this && this.__read) || function (o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+export var queryParamsToString = function (queryParams) {
+    return Object.keys(queryParams)
+        .reduce(function (acc, param) { return __spreadArray(__spreadArray([], __read(acc), false), __read((typeof queryParams[param] !== "undefined"
+        ? ["".concat(param.toString(), "=").concat(encodeURI(queryParams[param].toString()))]
+        : [])), false); }, [])
+        .join("&");
+};
+//# sourceMappingURL=queryParamsToString.mjs.map
