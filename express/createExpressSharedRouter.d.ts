4721d8c15f6e8e025c882b7172450780dcef5915
actions <actions@github.com>
2023-09-29 13:35:05 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ express/createExpressSharedRouter.d.ts
@@ -0,0 +1,8 @@
+import type { IRoute, RequestHandler, Router } from "express";
+import type { PathParameters, UnknownSharedRoute } from "..";
+import { z } from "zod";
+import { ValidationOptions } from "../validations";
+import { ValueOf } from "../defineRoutes";
+type ExpressSharedRouterOptions = Pick<ValidationOptions, "skipInputValidation">;
+export declare const createExpressSharedRouter: <SharedRoutes extends Record<string, UnknownSharedRoute>, ExpressSharedRouter extends { [Route in keyof SharedRoutes]: (...handlers: RequestHandler<PathParameters<SharedRoutes[Route]["url"]>, z.TypeOf<ValueOf<SharedRoutes[Route]["responses"]>>, z.TypeOf<SharedRoutes[Route]["requestBodySchema"]>, z.TypeOf<SharedRoutes[Route]["queryParamsSchema"]>, any>[]) => IRoute; }>(sharedRoutes: SharedRoutes, expressRouter: Router, options?: ExpressSharedRouterOptions) => ExpressSharedRouter;
+export {};
