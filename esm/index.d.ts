3352145c6dff8559cc360f4b7fb027619e157c3a
actions <actions@github.com>
2024-10-22 14:36:30 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ esm/index.d.ts
@@ -0,0 +1,7 @@
+export { createCustomSharedClient } from "./createCustomSharedClient.mjs";
+export { configureCreateHttpClient } from "./configureCreateHttpClient.mjs";
+export type { HandlerCreator, HttpClient, HttpResponse, } from "./configureCreateHttpClient.mjs";
+export { defineRoutes, defineRoute, listRoutes } from "./defineRoutes.mjs";
+export type { HttpMethod, SharedRoute, UnknownSharedRoute, UnknownSharedRouteWithUrl, } from "./defineRoutes.mjs";
+export type { PathParameters, Url } from "./pathParameters.mjs";
+export { keys } from "./pathParameters.mjs";
---
+++ express/createExpressSharedRouter.d.ts
@@ -0,0 +1,11 @@
+import type { IRoute, RequestHandler, Router } from "express";
+import type { PathParameters, UnknownSharedRoute } from "..";
+import { z } from "zod";
+import { ValidationOptions } from "../validations";
+import { ValueOfIndexNumber } from "../defineRoutes";
+type ExpressSharedRouterOptions = Pick<ValidationOptions, "skipInputValidation">;
+interface ParsedQs {
+    [key: string]: undefined | string | string[] | ParsedQs | ParsedQs[];
+}
+export declare const createExpressSharedRouter: <SharedRoutes extends Record<string, UnknownSharedRoute>, ExpressSharedRouter extends { [Route in keyof SharedRoutes & string]: (...handlers: RequestHandler<PathParameters<SharedRoutes[Route]["url"]>, z.infer<ValueOfIndexNumber<SharedRoutes[Route]["responses"]>>, z.infer<SharedRoutes[Route]["requestBodySchema"]>, z.infer<SharedRoutes[Route]["queryParamsSchema"]> extends void ? ParsedQs : z.infer<SharedRoutes[Route]["queryParamsSchema"]>, any>[]) => IRoute; }>(sharedRoutes: SharedRoutes, expressRouter: Router, options?: ExpressSharedRouterOptions) => ExpressSharedRouter;
+export {};
