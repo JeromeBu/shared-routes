aa6e0d2bb802c95546f90847a1aaeb781bb6c72d
actions <actions@github.com>
2024-11-08 15:38:36 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ validations.d.ts
@@ -0,0 +1,26 @@
+import type { HandlerParams, HttpResponse } from "./configureCreateHttpClient";
+import type { UnknownSharedRoute } from "./defineRoutes";
+export type HttpClientOptions = {
+    skipInputValidation?: boolean;
+    skipResponseValidation?: boolean;
+    skipResponseValidationForStatuses?: number[];
+    onResponseSideEffect?: (response: HttpResponse<any, any>) => void;
+};
+type ExtractFromExisting<T, U extends T> = Extract<T, U>;
+type CheckedSchema = ExtractFromExisting<keyof UnknownSharedRoute, "queryParamsSchema" | "requestBodySchema" | "headersSchema" | "responses">;
+export declare const validateSchemaWithExplicitError: <R extends UnknownSharedRoute>({ checkedSchema, params, route, adapterName, responseStatus, withIssuesInMessage, }: {
+    checkedSchema: ExtractFromExisting<keyof UnknownSharedRoute, "queryParamsSchema" | "requestBodySchema" | "headersSchema" | "responses">;
+    params: unknown;
+    route: R;
+    adapterName: string;
+    responseStatus?: CheckedSchema extends "responses" ? keyof R["responses"] : never;
+    withIssuesInMessage?: boolean;
+}) => unknown;
+export declare const validateInputParams: (route: UnknownSharedRoute, params: HandlerParams<UnknownSharedRoute>, adapterName: string, options?: {
+    withIssuesInMessage: boolean;
+}) => {
+    queryParams: unknown;
+    body: unknown;
+    headers: unknown;
+};
+export {};
